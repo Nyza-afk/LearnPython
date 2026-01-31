7f4221b36f5f1387571cc4dad3bc19ab5e4ca0b1
Nyza-afk <bm25mtech11007@iith.ac.in>
2026-01-31 20:07:40 +0530

    Add files via upload

---
+++ Maths.py
@@ -0,0 +1,19 @@
+#input from user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Addition
+sum = num1+num2
+print("Addition: ",sum)
+
+#Subtraction
+diff = num1-num2
+print("Subtraction: ",diff)
+
+#Multiplication
+mul = num1*num2
+print("Multiplication: ",mul)
+
+#Division
+div = num1/num2
+print("Division: ",div)
